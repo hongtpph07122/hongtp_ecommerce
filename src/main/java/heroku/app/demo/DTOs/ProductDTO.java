a02348a62ad74297337bbcb5194025ac5d2b7b7c
Hongtpph07122 <tranphuchong1435@gmail.com>
2022-08-11 14:01:32 +0700

    Create ProductDTO

--- src/main/java/heroku/app/demo/DTOs/ProductDTO.java
+++ src/main/java/heroku/app/demo/DTOs/ProductDTO.java
@@ -1,4 +1,22 @@
 package heroku.app.demo.DTOs;
 
+import heroku.app.demo.Entities.Category;
+import heroku.app.demo.Entities.Material;
+import heroku.app.demo.Entities.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+    private int isActive;
+    private CategoryDTO category;
+    private MaterialDTO material;
+    private TagDTO tag;
 }
